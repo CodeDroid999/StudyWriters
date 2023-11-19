c754ab50b8b241b2e70899b25d03423500839882
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-19 20:50:29 +0300

    Why bother

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
-import BeYourOwnBoss from 'components/home/BeYourOwnBoss'
-import BlogSection from 'components/home/BlogSection'
-import Features from 'components/home/Features'
-import Hero from 'components/home/Hero'
-import PostYourTask from 'components/home/PostYourTask'
-import Layout from 'components/layout/Layout'
-import React, { useState } from 'react'
+import HeroArea from 'components/Homepage/HeroSection'
+import HowItWorksSection from 'components/Homepage/HowITWorksSection'
+import PostYourAssignment from 'components/Homepage/PostYourAssignment'
+import ReferFriends from 'components/Homepage/ReferFriensSection.tsx'
+import StatsCounter from 'components/Homepage/StatsCounter'
+import Footer from 'components/layout/Footer'
 import { readToken } from 'lib/sanity.api'
 import { getAllPosts, getClient, getSettings } from 'lib/sanity.client'
 import { Post, Settings } from 'lib/sanity.queries'
-import type { SharedPageProps } from 'pages/_app'
-import FAQAccordion from 'components/FAQaccordions'
 import Head from 'next/head'
+import type { SharedPageProps } from 'pages/_app'
+import React, { useState } from 'react'
 
 interface PageProps extends SharedPageProps {
   posts: Post[]
@@ -25,44 +24,37 @@ export default function Home(props: PageProps) {
   const { posts, settings, draftMode } = props
 
   return (
-    <Layout>
-       <Head>
+    <>
+      <Head>
         <title>
-          Airtaska | Get More Done | Post any task. Pick the best person. Get it done. | Post your task for free Earn money as a tasker
+          QualityUnitedWriters - Your Academic Research and Project Partner
         </title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
-          content="Airtaska is your one-stop destination for finding the right tasks and talented taskers. Post any task, pick the best person, and get it done. Join now to earn money as a tasker or post your tasks for free."
+          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
         />
-        <meta name="keywords" content="Airtaska, tasks, tasker, earn money, post task" />
-        <meta name="author" content="Airtaska" />
+        <meta name="keywords" content="Academic writing services, Expert academic writers, Professional research assistance, High-quality research papers, Academic project support, Thesis and dissertation help, Essay writing service, Top-rated tutors, Academic success tips, Homework assistance, Online tutoring, Quality writing solutions, Best essay writers, Custom research papers, Academic support platform, Tutoring for students, Research paper editing, Writing and editing services, Academic guidance, Homework help for students" />
+        <meta name="author" content="QualityUnitedWriters" />
         <meta name="robots" content="index, follow" />
-        <meta name="og:title" property="og:title" content="Airtaska | Get More Done" />
+        <meta name="og:title" property="og:title" content="QualityUnitedWriters - Your Academic Research and Project Partner" />
         <meta
           name="og:description"
           property="og:description"
-          content="Airtaska is your one-stop destination for finding the right tasks and talented taskers. Post any task, pick the best person, and get it done. Join now to earn money as a tasker or post your tasks for free."
+          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
         />
         <meta name="og:image" property="og:image" content="public/airtaskalogo.jpeg" />
-        <meta name="og:url" property="og:url" content="https://www.airtaska.com" />
+        <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
       </Head>
-      <div className="px-3 sm:px-4 lg:px-6 xl:px-12">
-        <Hero />
-        <PostYourTask />
-        <Features />
-      </div>
-      <div className="lg:px-6 xl:px-12">
-        <BeYourOwnBoss />
-      </div>
-      <div className="my-8 bg-blue-50 md:my-16">
-        <BlogSection posts={posts} />
-      </div>
-      <div className="bg-neutral-100 lg:px-6 xl:px-12">
-        <FAQAccordion />
-      </div>
-    </Layout>
+      <HeroArea />
+      <StatsCounter/>
+      <PostYourAssignment />
+    
+      <ReferFriends/>
+      <HowItWorksSection />
+      <Footer />
+    </>
   )
 }
 
