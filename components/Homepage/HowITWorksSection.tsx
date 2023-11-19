9a7ecc76b44a15a2d4eb883d7bd053a7bca68d4e
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-19 21:15:23 +0300

    import Steps from './steps';

--- components/Homepage/HowITWorksSection.tsx
+++ components/Homepage/HowITWorksSection.tsx
@@ -1,7 +1,7 @@
+import Steps from 'components/howitworks/Steps';
 import Image from 'next/image'
 import HeroBackground from "public/main_cover.jpg"
 import React from 'react';
-import Steps from './steps';
 
 const HowItWorksSection = () => {
     return (
