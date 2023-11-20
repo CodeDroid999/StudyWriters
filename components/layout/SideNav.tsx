b9f82ea8cbaeda2a72c72156f0370f7f7e8eeaa7
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-20 12:24:56 +0300

    import Link from 'next/link';

--- components/layout/SideNav.tsx
+++ components/layout/SideNav.tsx
@@ -1,36 +1,38 @@
-// SideNav.js
 
+
+
+import Link from 'next/link';
 import React from 'react';
 
 const SideNav = () => {
   return (
     <div className="flex flex-col sidenav">
       <div className="p-2 flex items-center">
-        <link href="/help" className="text-blue-400 text-left whitespace-nowrap">Help</link>
+        <Link href="/help" className="text-blue-400 text-left whitespace-nowrap">Help</Link>
       </div>
       <div className="p-2 flex items-center">
-        <link href="/about" className="text-blue-400 text-left whitespace-nowrap">About</link>
+        <Link href="/about" className="text-blue-400 text-left whitespace-nowrap">About</Link>
       </div>
       <div className="p-2 flex items-center">
-        <link href="/privacy" className="text-blue-400 text-left whitespace-nowrap">Privacy</link>
+        <Link href="/privacy" className="text-blue-400 text-left whitespace-nowrap">Privacy</Link>
       </div>
       <div className="p-2 flex items-center">
-        <link href="/terms" className="text-blue-400 text-left whitespace-nowrap">Terms</link>
+        <Link href="/terms" className="text-blue-400 text-left whitespace-nowrap">Terms</Link>
       </div>
       <div className="p-2 flex items-center">
-        <link href="/refund" className="text-blue-400 text-left whitespace-nowrap">Refund</link>
+        <Link href="/refund" className="text-blue-400 text-left whitespace-nowrap">Refund</Link>
       </div>
       <div className="p-2 flex items-center">
-        <link href="/refer-a-friend" className="text-blue-400 text-left whitespace-nowrap">Refer a Friend</link>
+        <Link href="/refer-a-friend" className="text-blue-400 text-left whitespace-nowrap">Refer a Friend</Link>
       </div>
       <div className="p-2 flex items-center">
-        <link href="/blog" className="text-blue-400 text-left whitespace-nowrap">Blog</link>
+        <Link href="/blog" className="text-blue-400 text-left whitespace-nowrap">Blog</Link>
       </div>
       <div className="p-2 flex items-center">
-        <link href="/how-it-works" className="text-blue-400 text-left whitespace-nowrap">How It Works</link>
+        <Link href="/how-it-works" className="text-blue-400 text-left whitespace-nowrap">How It Works</Link>
       </div>
       <div className="p-2 flex items-center">
-        <link href="/contact-us" className="text-blue-400 text-left whitespace-nowrap">Contact Us</link>
+        <Link href="/contact-us" className="text-blue-400 text-left whitespace-nowrap">Contact Us</Link>
       </div>
     </div>
   );
