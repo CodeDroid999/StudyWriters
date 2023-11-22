1cb1cbabde68af1041c9e0d54fe1c2f05c027503
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-22 17:26:07 +0300

    perfect post tasks

--- components/Homepage/PostAssignment.tsx
+++ components/Homepage/PostAssignment.tsx
@@ -1,126 +1,212 @@
-import React, { useState, useEffect } from 'react';
-import { toast } from 'react-hot-toast';
-import { collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
-import { db } from '../../firebase';
-import router from 'next/router';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 import { UserAuth } from 'context/AuthContext';
-
-
+import { db } from '../../firebase';
+import {
+  addDoc,
+  collection,
+  getDocs,
+  serverTimestamp,
+} from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 interface Props {
-    handleNextStep: () => void
+  handleNextStep: () => void;
 }
+
 export default function PostAssignment() {
-    const { user } = UserAuth();
-    const userId = user?.userId;
-    const [selectedSubjects, setSelectedSubjects] = useState([]);
-    const [selectedRate, setSelectedRate] = useState('$10');
-    const [title, setTitle] = useState('')
-    const [titleError, setTitleError] = useState('')
-    const [dueDate, setDueDate] = useState('')
-    const [dueDateError, setDueDateError] = useState('')
+  const { user } = UserAuth();
+  const userId = user?.userId;
 
+  const [title, setTitle] = useState('');
+  const [titleError, setTitleError] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [dueDateError, setDueDateError] = useState('');
+  const [budget, setBudget] = useState('');
+  const [budgetError, setBudgetError] = useState('');
 
-    const handleSave = async () => {
-        // Validate form data
-        if (!selectedSubjects.length || !selectedRate) {
-            toast.error('Please fill in all required fields');
-            return;
+  const handleSave = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // Check if the user is logged in
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+      // User is not logged in, show toast error
+      toast.error('You are not logged in. Signup and log in to post your assignment');
+      return;
+    }
+
+    let hasError = false;
+
+    // Validate title
+    if (!title) {
+      setTitleError('This field is required');
+      hasError = true;
+    } else if (title.length < 10) {
+      setTitleError('Must be at least 10 characters');
+      hasError = true;
+    } else {
+      setTitleError('');
+    }
+
+    // Validate dueDate
+    if (!dueDate) {
+      setDueDateError('This field is required');
+      hasError = true;
+    } else {
+      setDueDateError('');
+    }
+
+    // Validate budget
+    if (!budget) {
+      setBudgetError('This field is required');
+      hasError = true;
+    } else {
+      const budgetValue = Number(budget);
+      if (isNaN(budgetValue) || budgetValue < 5 || budgetValue > 9999) {
+        setBudgetError('The price must be between $5 and $9999');
+        hasError = true;
+      } else {
+        setBudgetError('');
+      }
+    }
+
+    if (hasError) {
+      return;
+    }
+
+    try {
+      const docRef = await addDoc(collection(db, 'assignments'), {
+        title: title,
+        dueDate: dueDate,
+        budget: budget,
+        status: 'Open',
+        createdAt: serverTimestamp(),
+        student: {
+          userId: userId,
+          price: '',
+          bookingFee: '',
+          finalPrice: '',
+        },
+        tutor: {
+          userId: '',
+          price: '',
+          serviceFee: '',
+          finalPrice: '',
+          proposal: '',
+        },
+        paymentRequested: false,
+        paymentReleased: false,
+        studentReview: false,
+        tutorReview: false,
+      });
+
+      const assignmentId = docRef.id;
+      const usersCollection = collection(db, 'users');
+      const querySnapshot = await getDocs(usersCollection);
+
+      const userEmails: string[] = [];
+
+      querySnapshot.forEach((doc) => {
+        const userData = doc.data();
+        if (userData.email) {
+          userEmails.push(userData.email);
         }
+      });
 
-        try {
-            const q = query(collection(db, 'assignments'), where('userId', '==', userId));
-            const querySnapshot = await getDocs(q);
+      await addDoc(collection(db, 'mail'), {
+        to: 'qualityunited340@gmail.com',
+        bcc: userEmails,
+        message: {
+          subject: 'New Assignment',
+          html: `A new assignment has been posted`,
+        },
+      });
 
-            if (!querySnapshot.empty) {
-                const docSnapshot = querySnapshot.docs[0];
-                const userDocRef = doc(db, 'users', docSnapshot.id);
-                await updateDoc(userDocRef, {
-                    subjects: selectedSubjects,
-                    rate: selectedRate,
-                });
-                toast.success('Subject preferences and rates saved!');
-                router.push('/tutor-application/step3');
-            } else {
-                toast.error('User document not found');
-            }
-        } catch (error) {
-            console.error('Error updating personal info:', error.message);
-            toast.error('Error updating personal info. Please try again.');
-        }
-    };
+      toast.success('Assignment has been posted');
+      // You can redirect to the assignment page or do any other necessary action
+      // router.push(`/assignments/${assignmentId}`);
+    } catch (error) {
+      console.error('Error posting assignment:', error.message);
+      toast.error('Error posting assignment. Please try again.');
+    }
+  };
 
-    const currentDate = new Date().toISOString().split('T')[0]
+  const currentDate = new Date().toISOString().split('T')[0];
 
-    return (
-        <div className="w-full bg-gray-100">
-
-            <p className="pt-3 text-center text-3xl font-bold text-green-950">
-                Get Homework Help
-            </p>
-            <p className="text-gray text-center">
-                Find a tutor to help you with your school!
-            </p>
-            <form className="mt-6 flex justify-between gap-4 md:mt-8">
-                <div className="flex flex-col col-md-3">
-                    <label
-                        htmlFor="title"
-                        className="mb-2 text-lg font-medium text-gray-700"
-                    >
-                        What do you need done?
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="e.g.Dissertation writing for Engineering Paper "
-                        onChange={(e) => setTitle(e.target.value)}
-                        className="border rounded p-1"
-                    />
-                    {titleError && <span className="text-red-500">{titleError}</span>}
-                </div>
-                <div className="flex flex-col col-md-3">
-                    <label
-                        htmlFor="description"
-                        className="mb-2 text-lg font-medium text-gray-700"
-                    >
-                        When do you need this done?
-                    </label>
-                    <input
-                        type="date"
-                        placeholder="Enter date"
-                        min={currentDate}
-                        onChange={(e) => setDueDate(e.target.value)}
-                        className="border rounded p-1"
-                    />
-                    {dueDateError && <span className="text-red-500">{dueDateError}</span>}
-                </div>
-                <div className="mb-4">
-                    <label className="text-sm font-medium text-gray-700">
-                        Preferred Rates
-                    </label>
-                    <div className="flex items-center mt-2 space-x-2">
-                        <select
-                            value={selectedRate}
-                            onChange={(e) => setSelectedRate(e.target.value)}
-                            className="border rounded p-1"
-                        >
-                            {Array.from({ length: 15 }, (_, index) => (index + 1) * 10).map((rate) => (
-                                <option key={rate} value={`$${rate}`}>
-                                    {`$${rate} per hour`}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                </div>
-
-                <div className="col-md-3 flex justify-left align-center items-center">
-                    <div
-                        className="btn-1 bg-yellow-500 p-2 rounded text-white  "
-                        onClick={handleSave}
-                    >
-                        Post Assignment
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className="w-full bg-gray-100 pb-4">
+      <p className="pt-3 text-center text-3xl font-bold text-green-950">
+        Get Homework Help
+      </p>
+      <p className="text-gray text-center">
+        Find a tutor to help you with your school!
+      </p>
+      <form className="mt-6 flex justify-between gap-4 md:mt-8">
+        <div className="flex flex-col col-md-3">
+          <label
+            htmlFor="title"
+            className="mb-2 text-lg font-medium text-gray-700"
+          >
+            What do you need done?
+          </label>
+          <input
+            type="text"
+            placeholder="e.g. Dissertation writing for Engineering Paper"
+            onChange={(e) => setTitle(e.target.value)}
+            className="border rounded p-1"
+          />
+          {titleError && (
+            <span className="text-red-500">{titleError}</span>
+          )}
         </div>
-    )
+        <div className="flex flex-col col-md-3">
+          <label
+            htmlFor="dueDate"
+            className="mb-2 text-lg font-medium text-gray-700"
+          >
+            When do you need this done?
+          </label>
+          <input
+            type="date"
+            id="dueDate"
+            required
+            placeholder="Enter date"
+            min={currentDate}
+            onChange={(e) => setDueDate(e.target.value)}
+            className="border rounded p-1"
+          />
+          {dueDateError && (
+            <span className="text-red-500">{dueDateError}</span>
+          )}
+        </div>
+        <div className="col-md-3 flex flex-col ">
+          <label
+            htmlFor="budget"
+            className="mb-2 text-lg font-medium text-gray-700"
+          >
+            Willing to pay
+          </label>
+          <input
+            placeholder="$ Enter budget"
+            onChange={(e) => setBudget(e.target.value)}
+            className="border rounded p-1"
+          />
+          {budgetError && (
+            <span className="text-red-500">{budgetError}</span>
+          )}
+        </div>
+        <div className="col-md-3 flex justify-start align-right items-center">
+          <div
+            className="btn-1 bg-yellow-500 p-2 rounded text-white"
+            onClick={handleSave}
+          >
+            Post Assignment
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
