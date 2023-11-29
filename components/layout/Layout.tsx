02b87a1c1bb79f786828e522ea09867ec43bfa6a
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-29 12:57:03 +0300

    add head

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -7,7 +7,7 @@ import Head from 'next/head'
 export default function Layout({ children }) {
   return (
     <div>
-     <Head>
+      <Head>
         <title>
           QualityUnitedWriters - Your Academic Research and Project Partner
         </title>
@@ -28,57 +28,6 @@ export default function Layout({ children }) {
         />
         <meta name="og:image" property="og:image" content="public/airtaskalogo.jpeg" />
         <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
-      </Head>|<Head>
-        <title>
-          QualityUnitedWriters - Your Academic Research and Project Partner
-        </title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="keywords" content="Academic writing services, Expert academic writers, Professional research assistance, High-quality research papers, Academic project support, Thesis and dissertation help, Essay writing service, Top-rated tutors, Academic success tips, Homework assistance, Online tutoring, Quality writing solutions, Best essay writers, Custom research papers, Academic support platform, Tutoring for students, Research paper editing, Writing and editing services, Academic guidance, Homework help for students" />
-        <meta name="author" content="QualityUnitedWriters" />
-        <meta name="robots" content="index, follow" />
-        <meta name="og:title" property="og:title" content="QualityUnitedWriters - Your Academic Research and Project Partner" />
-        <meta
-          name="og:description"
-          property="og:description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="og:image" property="og:image" content="public/airtaskalogo.jpeg" />
-        <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
-      </Head>| Post any assignment. Pick the best person. Get it
-          done. | Post your assignment for free Earn money as a tutor
-        </title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/public/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/public/favicon/favicon-16x16.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/public/favicon/favicon-32x32.png"
-        />
-        <meta
-          name="description"
-          content="Post your assignment for free | Earn money as a tutor| Discover how Airtaska works | Trusted ratings and reviews| Insurance for peace of mind | Join Airtaska today!"
-        />
-        <link
-          href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css"
-          rel="stylesheet"
-        />
       </Head>
       <Navbar />
       <main className="mt-20 lg:mt-24">{children}</main>
--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -43,111 +43,27 @@ export default function ForgotPassword() {
 
   return (
     <AuthLayout>
-     <Head>
+      <Head>
         <title>
-          QualityUnitedWriters - Your Academic Research and Project Partner
+          Airtaska | Get More Done | Post any assignment. Pick the best person. Get it done. | Post your assignment for free Earn money as a tutor
         </title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
+          content="Airtaska is your one-stop destination for finding the right assignments and talented taskers. Post any assignment, pick the best person, and get it done. Join now to earn money as a tutor or post your assignments for free."
         />
-        <meta name="keywords" content="Academic writing services, Expert academic writers, Professional research assistance, High-quality research papers, Academic project support, Thesis and dissertation help, Essay writing service, Top-rated tutors, Academic success tips, Homework assistance, Online tutoring, Quality writing solutions, Best essay writers, Custom research papers, Academic support platform, Tutoring for students, Research paper editing, Writing and editing services, Academic guidance, Homework help for students" />
-        <meta name="author" content="QualityUnitedWriters" />
+        <meta name="keywords" content="Airtaska, assignments, tutor, earn money, post assignment" />
+        <meta name="author" content="Airtaska" />
         <meta name="robots" content="index, follow" />
-        <meta name="og:title" property="og:title" content="QualityUnitedWriters - Your Academic Research and Project Partner" />
+        <meta name="og:title" property="og:title" content="Airtaska | Get More Done" />
         <meta
           name="og:description"
           property="og:description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
+          content="Airtaska is your one-stop destination for finding the right assignments and talented taskers. Post any assignment, pick the best person, and get it done. Join now to earn money as a tutor or post your assignments for free."
         />
         <meta name="og:image" property="og:image" content="public/airtaskalogo.jpeg" />
-        <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
-      </Head>|<Head>
-        <title>
-          QualityUnitedWriters - Your Academic Research and Project Partner
-        </title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="keywords" content="Academic writing services, Expert academic writers, Professional research assistance, High-quality research papers, Academic project support, Thesis and dissertation help, Essay writing service, Top-rated tutors, Academic success tips, Homework assistance, Online tutoring, Quality writing solutions, Best essay writers, Custom research papers, Academic support platform, Tutoring for students, Research paper editing, Writing and editing services, Academic guidance, Homework help for students" />
-        <meta name="author" content="QualityUnitedWriters" />
-        <meta name="robots" content="index, follow" />
-        <meta name="og:title" property="og:title" content="QualityUnitedWriters - Your Academic Research and Project Partner" />
-        <meta
-          name="og:description"
-          property="og:description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="og:image" property="og:image" content="public/airtaskalogo.jpeg" />
-        <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
-      </Head>|<Head>
-        <title>
-          QualityUnitedWriters - Your Academic Research and Project Partner
-        </title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="keywords" content="Academic writing services, Expert academic writers, Professional research assistance, High-quality research papers, Academic project support, Thesis and dissertation help, Essay writing service, Top-rated tutors, Academic success tips, Homework assistance, Online tutoring, Quality writing solutions, Best essay writers, Custom research papers, Academic support platform, Tutoring for students, Research paper editing, Writing and editing services, Academic guidance, Homework help for students" />
-        <meta name="author" content="QualityUnitedWriters" />
-        <meta name="robots" content="index, follow" />
-        <meta name="og:title" property="og:title" content="QualityUnitedWriters - Your Academic Research and Project Partner" />
-        <meta
-          name="og:description"
-          property="og:description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="og:image" property="og:image" content="public/airtaskalogo.jpeg" />
-        <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
-      </Head>|<Head>
-        <title>
-          QualityUnitedWriters - Your Academic Research and Project Partner
-        </title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="keywords" content="Academic writing services, Expert academic writers, Professional research assistance, High-quality research papers, Academic project support, Thesis and dissertation help, Essay writing service, Top-rated tutors, Academic success tips, Homework assistance, Online tutoring, Quality writing solutions, Best essay writers, Custom research papers, Academic support platform, Tutoring for students, Research paper editing, Writing and editing services, Academic guidance, Homework help for students" />
-        <meta name="author" content="QualityUnitedWriters" />
-        <meta name="robots" content="index, follow" />
-        <meta name="og:title" property="og:title" content="QualityUnitedWriters - Your Academic Research and Project Partner" />
-        <meta
-          name="og:description"
-          property="og:description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="og:image" property="og:image" content="public/airtaskalogo.jpeg" />
-        <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
-      </Head>|<Head>
-        <title>
-          QualityUnitedWriters - Your Academic Research and Project Partner
-        </title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="keywords" content="Academic writing services, Expert academic writers, Professional research assistance, High-quality research papers, Academic project support, Thesis and dissertation help, Essay writing service, Top-rated tutors, Academic success tips, Homework assistance, Online tutoring, Quality writing solutions, Best essay writers, Custom research papers, Academic support platform, Tutoring for students, Research paper editing, Writing and editing services, Academic guidance, Homework help for students" />
-        <meta name="author" content="QualityUnitedWriters" />
-        <meta name="robots" content="index, follow" />
-        <meta name="og:title" property="og:title" content="QualityUnitedWriters - Your Academic Research and Project Partner" />
-        <meta
-          name="og:description"
-          property="og:description"
-          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
-        />
-        <meta name="og:image" property="og:image" content="public/airtaskalogo.jpeg" />
-        <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
+        <meta name="og:url" property="og:url" content="https://www.QualityUnited Writers.com" />
       </Head>
 
       <div className="align-center flex flex-col justify-center">
