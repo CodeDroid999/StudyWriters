f72e645cbcfb6216991900e1ceffc08a697c4a66
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-30 10:58:51 +0300

    rush

---
+++ pages/my-assignments/id.tsx
@@ -0,0 +1,160 @@
+import Navbar from 'components/layout/Navbar'
+import { db, auth } from '../../firebase'
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { formatDate } from 'pages/profile/[id]'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useRouter } from 'next/router'
+import { UserAuth } from 'context/AuthContext'
+import MyAssignments from 'components/my-assignments/myAssignments'
+
+export default function MyAssignmentsPage() {
+  const [selectedFilter, setSelectedFilter] = useState('')
+  const [assignments, setassignments] = useState([])
+  const [loading, setLoading] = useState(false)
+  const { user } = UserAuth()
+
+  const router = useRouter()
+  const userId = router.query.id?.toString()
+
+  useEffect(() => {
+    setLoading(true)
+    const q = query(collection(db, 'assignments'))
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const updatedassignments = []
+
+      querySnapshot.forEach(async (doc) => {
+        const data = doc.data()
+
+        const id = doc.id
+
+        const offersCollectionRef = collection(db, 'assignments', id, 'offers')
+        const offersQuerySnapshot = await getDocs(offersCollectionRef)
+        const offers = offersQuerySnapshot.docs.map((offerDoc) => {
+          const offerData = offerDoc.data()
+
+          return offerData
+        })
+
+        updatedassignments.push({ id, ...data, offers })
+      })
+
+      setassignments(updatedassignments)
+      setLoading(false)
+    })
+
+    return () => {
+      unsubscribe() // Clean up the listener when the component unmounts
+    }
+  }, [userId])
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.push(`/`)
+      }
+    })
+    return () => unsubscribe()
+  }, [router])
+  const postedAssignments = assignments.filter((task) => task.poster.userId === userId)
+  const assignedassignments = assignments.filter(
+    (task) => task.tasker.userId === userId && task.status === 'Assigned'
+  )
+  const completedassignments = assignments.filter(
+    (task) => task.tasker.userId === userId && task.status === 'Completed'
+  )
+  const pendingOffers = assignments.filter((task) => {
+    return (
+      task.status === 'Open' &&
+      task.offers.some((offer: any) => offer.userId === userId)
+    )
+  })
+
+  return (
+    <div>
+      <Navbar />
+      {loading ? (
+        <div className="flex h-screen items-center justify-center">
+          <div
+            className="inline-block h-6 w-6 animate-spin rounded-full border-[3px] border-current border-t-transparent text-blue-600"
+            role="status"
+            aria-label="loading"
+          >
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className=" mt-28 px-3">
+          <div className="flex flex-row justify-end">
+            <div>
+              <label
+                htmlFor="filter"
+                className="mr-1 text-xl font-medium text-green-950"
+              >
+                Category:
+              </label>
+              <select
+                id="filter"
+                value={selectedFilter}
+                onChange={(e) => setSelectedFilter(e.target.value)}
+                className="rounded-md border-2 border-blue-950 font-medium text-blue-900 outline-blue-900 "
+              >
+                <option value="">Select Filter</option>
+                <option value="posted">Posted assignments</option>
+                <option value="assigned">assignments Assigned</option>
+                <option value="offers-pending">Offers Pending</option>
+                <option value="completed">assignments Completed</option>
+              </select>
+            </div>
+          </div>
+          <div className="mt-5">
+            {!selectedFilter && (
+              <div className="mt-28 flex flex-col items-center justify-center">
+                <h1 className="text-xl font-medium text-gray-700">
+                  Hello {user?.firstName}, select a category to display your
+                  assignments
+                </h1>
+              </div>
+            )}
+            {selectedFilter === 'posted' && (
+              <MyAssignments
+                heading="Posted assignments"
+                assignments={postedAssignments}
+                warning="You have not posted any assignments!"
+              />
+            )}
+            {selectedFilter === 'assigned' && (
+              <MyAssignments
+                heading="assignments I have been assigned"
+                assignments={assignedassignments}
+                warning="You have not been assigned any assignments!"
+              />
+            )}
+            {selectedFilter === 'offers-pending' && (
+              <MyAssignments
+                heading="My active offers"
+                assignments={pendingOffers}
+                warning="You have no pending offers!"
+              />
+            )}
+            {selectedFilter === 'completed' && (
+              <MyAssignments
+                heading="assignments I have completed"
+                assignments={completedassignments}
+                warning="You have not completed any assignments!"
+              />
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
