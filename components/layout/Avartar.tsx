b7669d830a948fe1ba653a04927376eaf0777ba3
CodeDroid99 <johnmwega2002@gmail.com>
2023-12-02 06:54:38 +0300

    bsdbksdbdhj

--- components/layout/Avartar.tsx
+++ components/layout/Avartar.tsx
@@ -39,8 +39,8 @@ export default function Avartar() {
                 <Image
                   src={user?.profilePicture || profile}
                   alt="profile"
-                  width={25}
-                  height={25}
+                  width={50}
+                  height={50}
                   className="h-[1.6rem] w-[1.6rem] cursor-pointer rounded-full object-cover"
                 />
                 {user?.firstName} {user?.lastName}
