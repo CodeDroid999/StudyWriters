ca88e2089c8613e77fdda7c183507dc857bde226
CodeDroid99 <johnmwega2002@gmail.com>
2023-12-23 14:37:57 +0300

    two vvs

--- components/Homepage/HowITWorksSection.tsx
+++ components/Homepage/HowITWorksSection.tsx
@@ -5,8 +5,10 @@ import React from 'react';
 
 const HowItWorksSection = () => {
     return (
-        <div className="container mx-auto pl-5">
-            <h1 className="text-green-950 text-center text-3xl font-bold pt-3">How it works</h1>
+        <div className="container mx-auto pl-5 bg-gray-100 pt-4">
+            <h1 className="font-bold text-center md:text-6xl text-4xl text-green-950 md:pt-3 whitespace-nowrap">
+                How it works
+            </h1>
             <p className=" text-center text-lg">
                 Thousands of college students have used QualityUnitedWriters as their secret weapon to make their life easier.
                 <br></br>Now it is your turn.                        </p>
--- components/howitworks/Steps.tsx
+++ components/howitworks/Steps.tsx
@@ -12,83 +12,73 @@ import photo3 from "../../public/pay.png"
 export default function StudentSteps() {
     return (
         <div>
-            <footer className="md:m-4   px-2 py-2">
-                <div className="row mt-2 grid gap-y-2 sm:mt-6 sm:flex sm:items-center sm:justify-between sm:gap-y-0">
-                    <div className="col-md-4 col-sm-12 flex flex-col items-center justify-center">
-                        <div className="mb-1">
-                            <Image
-                                src={photo1}
-                                alt="assignment"
-                                className="h-[150px] w-[100%]"
-                            />
-                        </div>
-                        <div>
-
-                            <span className=" font-bold text-3xl text-center text-gray-600">
-                                <p className="pt-1 pb-2 text-lg"> 1. Upload</p>
-                            </span>
-
-                            <p className=" font-bold text-sm text-center text-gray-600 w-full">
-                                your question for the tutor
-                            </p>
-                        </div>
-
+            <div className="row mt-2 space-x-2 justify-evenly">
+                <div className="col-md-3 col-sm-10 flex flex-col items-center justify-center border shadow p-2 rounded">
+                    <div className="mb-1">
+                        <Image
+                            src={photo1}
+                            alt="assignment"
+                            className="h-[150px] w-[100%]"
+                        />
                     </div>
-                    <div className="col-md-4 col-sm-12 flex flex-col items-center justify-center">
-                        <div className="mb-1">
-                            <Image
-                                src={photo2}
-                                alt="assignment"
-                                className="h-[150px] w-[100%]"
-                            />
-                        </div>
-                        <div>
+                    <div>
 
-                            <span className=" font-bold text-4xl text-center text-gray-600">
-                                <p className="pt-1 pb-2 text-lg">2. Pick</p>
-                            </span>
-
-                            <p className=" font-bold text-sm text-center text-gray-600 w-full">
-                                a bidder                            </p>
-                        </div>
+                        <span className=" font-bold text-3xl text-center text-gray-600">
+                            <p className="pt-1 pb-2 text-lg"> 1. Upload your question</p>
+                        </span>
 
                     </div>
 
-                    <div className="col-md-4 col-sm-12 flex flex-col items-center justify-center">
-                        <div className="mb-1">
-                            <Image
-                                src={photo3}
-                                alt="assignment"
-                                className="h-[150px] w-[100%]"
-                            />
-                        </div>
-                        <div>
-
-                            <span className=" font-bold text-3xl text-center text-gray-600">
-                                <p className="pt-1 pb-2 text-lg"> 3. Pay</p>
-                            </span>
-
-                            <p className=" font-bold text-sm text-center text-gray-600 w-full">
-                                after help is given                            </p>
-                        </div>
-
+                </div>
+                <div className="col-md-3 col-sm-10 flex flex-col items-center justify-center border shadow p-2 rounded">
+                    <div className="mb-1">
+                        <Image
+                            src={photo2}
+                            alt="assignment"
+                            className="h-[150px] w-[100%]"
+                        />
                     </div>
-                    <div className="col-md-12 col-sm-12 flex flex-col items-center justify-center pt-3 pb-2">
-                        <div className="mb-1">
-                            <Link href="/signup" className='flex justify-center items-centerdecoration-none '>
-                                <div className="w-60 text-center rounded p-2 bg-yellow-500 text-white text-lg">
-                                    Signup
-                                </div>
-                            </Link>
-                        </div>
+                    <div>
 
-
+                        <span className=" font-bold text-4xl text-center text-gray-600">
+                            <p className="pt-1 pb-2 text-lg">2. Pick a tutor</p>
+                        </span>
                     </div>
 
+                </div>
+
+                <div className="col-md-3 col-sm-10 flex flex-col items-center justify-center border shadow p-2 rounded">
+                    <div className="mb-1">
+                        <Image
+                            src={photo3}
+                            alt="assignment"
+                            className="h-[150px] w-[100%]"
+                        />
+                    </div>
+                    <div>
+
+                        <span className=" font-bold text-3xl text-center text-gray-600">
+                            <p className="pt-1 pb-2 text-lg"> 3. Pay after help</p>
+                        </span>
+
+                    </div>
+
+                </div>
+                <div className="col-md-12 col-sm-12 flex flex-col items-center justify-center pt-3 pb-2">
+                    <div className="mb-1">
+                        <Link href="/signup" className='flex justify-center items-centerdecoration-none '>
+                            <div className="w-60 text-center rounded p-2 bg-yellow-500 text-white text-lg">
+                                Signup
+                            </div>
+                        </Link>
+                    </div>
 
 
                 </div>
-            </footer>
+
+
+
+            </div>
         </div>
     )
 }
