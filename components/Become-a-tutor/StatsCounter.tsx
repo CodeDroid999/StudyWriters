72065804f53d3ad7ad8f08af94ab802f37fc405e
CodeDroid99 <johnmwega2002@gmail.com>
2023-12-23 11:29:49 +0300

    remove stats counter on mobile

--- components/Become-a-tutor/StatsCounter.tsx
+++ components/Become-a-tutor/StatsCounter.tsx
@@ -20,25 +20,25 @@ const StatsCounter = () => {
         <h2 className="text-3xl font-bold rounded-xl shadow-inner p-1">
           <CountUp end={48777} duration={2} start={startAnimation ? null : undefined} /> +
         </h2>
-        <p className="text-xl font-bold pt-2 pb-2">Visitors</p>
+        <p className="text-xl font-bold pt-2 pb-2 text-green-950">Visitors</p>
       </div>
       <div className="rounded text-center p-2 shadow">
         <h2 className="text-3xl font-bold rounded-xl shadow-inner p-1">
           <CountUp end={136050} duration={2} start={startAnimation ? null : undefined} /> +
         </h2>
-        <p className="text-xl font-bold pt-2 pb-2">CompletedAssignments</p>
+        <p className="text-xl font-bold pt-2 pb-2 text-green-950">CompletedAssignments</p>
       </div>
       <div className="rounded text-center p-2 shadow">
         <h2 className="text-3xl font-bold rounded-xl shadow-inner p-1">
           <CountUp end={9.37} duration={2} decimals={2} start={startAnimation ? null : undefined} /> +
         </h2>
-        <p className="text-xl font-bold pt-2 pb-2">Current Quality Score</p>
+        <p className="text-xl font-bold pt-2 pb-2 text-green-950">Current Quality Score</p>
       </div>
       <div className="rounded text-center p-2 shadow">
         <h2 className="text-3xl font-bold rounded-xl shadow-inner p-1">
           <CountUp end={2674} duration={2} start={startAnimation ? null : undefined} /> +
         </h2>
-        <p className="text-xl font-bold pt-2 pb-2">Writers Active</p>
+        <p className="text-xl font-bold pt-2 pb-2 text-green-950">Writers Active</p>
       </div>
     </div>
   );
