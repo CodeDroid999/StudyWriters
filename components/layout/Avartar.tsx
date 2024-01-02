d0023678749beb1519e755c59a2e1886e74d9b2f
CodeDroid99 <johnmwega2002@gmail.com>
2024-01-02 17:38:26 +0300

    perfect Avatarr

--- components/layout/Avartar.tsx
+++ components/layout/Avartar.tsx
@@ -6,6 +6,7 @@ import profile from 'public/profile.jpeg'
 
 export default function Avartar() {
   const { user, logOut } = UserAuth()
+  const userRole = user?.role
 
   const handleLogOut = () => {
     logOut()
@@ -39,79 +40,107 @@ export default function Avartar() {
                 <Image
                   src={user?.profilePicture || profile}
                   alt="profile"
-                  width={50}
-                  height={50}
+                  width={25}
+                  height={25}
                   className="h-[1.6rem] w-[1.6rem] cursor-pointer rounded-full object-cover"
                 />
                 {user?.firstName} {user?.lastName}
               </span>
             </Link>
           </div>
+          <div className="flex ">
+            {userRole === 'Student' && (
+              <Link href="/browse-assignments" className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 mt-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100">
+                Browse assignments
+              </Link>
+            )}
+
+            {userRole === 'Tutor' && (
+              <Link href="/boost-earnings" className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1  mt-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100">
+                Boost Earnings
+              </Link>
+            )}
+          </div>
+          <div className="flex ">
+            {userRole === 'Student' && (
+              <Link href="/post-assignments" className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100">
+                Post assignments
+              </Link>
+            )}
+
+            {userRole === 'Tutor' && (
+              <Link href="/bid-assignments" className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100">
+                Bid assignments
+              </Link>
+            )}
+          </div>
+          <div className="flex ">
+            {userRole === 'Student' && (
+              <Link href="/my-assignments/{userId}" className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100">
+                My assignments
+              </Link>
+            )}
+
+            {userRole === 'Tutor' && (
+              <Link href="/orders/{userId}" className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100">
+                My Orders
+              </Link>
+            )}
+          </div>
+
           <Link
-            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mt-1 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100   "
-            href="/become-a-tutor"
-          >
-            Answer Questions
-          </Link>
-          <Link
-            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mt-1 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100   "
-            href="/my-assignments"      >
-            My Answers
-          </Link>
-          <Link
-            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mt-1 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100   "
+            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100"
             href={`/profile/${user?.userId}`}
           >
             My Profile
           </Link>
           <Link
-            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mt-1 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100   "
+            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100  "
             href="/how-it-works"      >
             How it Works
           </Link>
           <Link
-            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mt-1 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100   "
+            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100  border w-100 "
             href={`/payment-history/${user?.userId}`}
           >
             Payment History
           </Link>
           <Link
-            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mt-1 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100   "
+            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100  "
             href={`/payment-methods/${user?.userId}`}
           >
             Payment Methods
           </Link>
-          <Link
-            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mt-1 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100   "
-            href="/how-it-works"      >
-            Honor Code          </Link>
-          <div className="">
+          <div className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100  ">
+
             <div
               onClick={toggleDropdown}
               className="mt-1 flex cursor-pointer items-center px-3  text-sm text-gray-800 hover:bg-gray-100 "
             >
               Settings
             </div>
-            {isDropdownOpen && (
-              <div className="ml-4 flex flex-col rounded p-1 text-sm text-gray-800 ">
-                <Link href={`/settings/mobile-number/${user?.userId}`}>
-                  Mobile
-                </Link>
-                <Link href={`/settings/email/${user?.userId}`}>Email</Link>
-                <Link href="/forgot-password">Change password</Link>
-              </div>
-            )}
           </div>
+
+          {isDropdownOpen && (
+            <div className="flex flex-col rounded p-1 text-sm text-gray-800 w-100 bg-gray-200">
+              <Link className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100 " href={`/settings/mobile-number/${user?.userId}`}>
+                Mobile
+              </Link>
+              <Link className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100 " href={`/settings/email/${user?.userId}`}>Email</Link>
+              <Link className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100 " href="/forgot-password">Change password</Link>
+            </div>
+          )}
+
 
           <div
             onClick={handleLogOut}
-            className="flex cursor-pointer items-center gap-x-3.5 rounded-md px-3 mt-1 mb-1 py-2 text-sm text-gray-800 hover:bg-gray-100  "
+            className="bg-white- 500 flex items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-neutral-100 border w-100  "
           >
             Log out
           </div>
           <div
             onClick={handleLogOut}
-            className="flex cursor-pointer items-center gap-x-3.5 rounded-md px-3 mt-1 mb-1 py-2 text-sm text-gray-800 hover:bg-gray-100  "
+            className="flex cursor-pointer items-center gap-x-3.5 rounded-md px-3 mb-1 py-2 text-sm text-gray-800 hover:bg-gray-100  border w-100 "
           >
             Swap to Student Mode
           </div>
