e6e079027b650e928c9eae9140ac3b10518e5588
CodeDroid99 <johnmwega2002@gmail.com>
2024-01-07 14:09:19 +0300

    perfect footer:inline-flex text-sm h-10 w-10

--- components/Become-a-tutor/PopularCountriesSection.tsx
+++ components/Become-a-tutor/PopularCountriesSection.tsx
@@ -15,20 +15,18 @@ const PopularCountries = () => {
     ];
 
     return (
-        <div className="sm:px-18  flex h-[50vh] flex-col-reverse items-center  bg-green-700  px-4 py-16  lg:px-24 xl:my-10 xl:flex-row  xl:px-36">
+        <div className="bg-green-700  px-4 py-16 ">
             <div className="container">
                 <div className="flex flex-1 flex-col items-center justify-center">
-                    <h1 className="text-center  text-2xl font-bold text-gray-100 pt-2 pb-4  ">
+                    <h1 className="text-center  md:text-2xl font-bold text-gray-100 pt-2 pb-4  ">
                         Popular Tutor Countries
                     </h1>
-                    <div className="row justify-between">
-                        <div className="col-sm-2 col-md-1 py-2">
-                            {popularCountries.map((country, index) => (
-                                <span key={index} className="py-2 px-2 text-lg text-white">
-                                    {country}
-                                </span>
-                            ))}
-                        </div>
+                    <div className="row justify-between md:border">
+                        {popularCountries.map((country, index) => (
+                            <span key={index} className="col-sm-2 md:col-md-1 py-2 px-2 md:text-lg text-gray-300 underline font-extrabold md:border">
+                                {country}
+                            </span>
+                        ))}
                     </div>
                 </div>
             </div>
