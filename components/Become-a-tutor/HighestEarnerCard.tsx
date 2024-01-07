394b366bc699bcfdafe13f84210d5ef190695d2e
CodeDroid99 <johnmwega2002@gmail.com>
2024-01-07 09:43:50 +0300

    git reset a8cae8d

--- components/Become-a-tutor/HighestEarnerCard.tsx
+++ components/Become-a-tutor/HighestEarnerCard.tsx
@@ -4,25 +4,24 @@ const HighestEarner = () => {
   // Sample data
   const earners = [
     { name: 'Allanmitch', rating: 5, earnings: 94372 },
-    { name: 'Parmajohn', rating: 5, earnings: 92447 },
-    { name: 'Chexmex', rating: 5, earnings: 80329 },
+    { name: 'Parmajohn', rating: 4, earnings: 92447 },
+    { name: 'Chexmex', rating: 4, earnings: 80329 },
     { name: 'jkim', rating: 5, earnings: 78669 },
-    { name: 'Dorime', rating: 5, earnings: 74615 },
   ];
 
   return (
-    <div className="flex md:space-x-10">
+    <div className="row  w-full bg-green-500">
       {earners.map((earner, index) => (
-        <div key={index} className="mb-6">
-          <h2 className="text-xl font-semibold text-blue-950">{earner.name}</h2>
+        <div key={index} className="col-md-3 col-sm-6 border">
+          <h2 className="text-center text-xl font-semibold text-blue-950">{earner.name}</h2>
           {/* Display star rating */}
-          <div className="flex mb-2">
+          <div className="flex mb-2 text-center w-full">
             {Array.from({ length: earner.rating }, (_, i) => (
-              <span key={i} className="text-yellow-500">&#9733;</span>
+              <span key={i} className="text-yellow-500 text-lg p-1 text-center">&#9733;</span>
             ))}
           </div>
           {/* Display earnings */}
-          <p className="text-lg">${earner.earnings.toFixed(2)}</p>
+          <p className="text-lg text-gray-200 text-center">$ {earner.earnings.toFixed(2)}</p>
         </div>
       ))}
     </div>
--- components/Become-a-tutor/HighestEarnersSection.tsx
+++ components/Become-a-tutor/HighestEarnersSection.tsx
@@ -3,17 +3,13 @@ import HighestEarnerCard from './HighestEarnerCard'
 
 export default function HighestEarners() {
     return (
-        <div className="sm:px-18  flex h-[50vh] flex-col-reverse items-center  bg-violet-100  px-4 py-16  lg:px-24 xl:my-10 xl:flex-row  xl:px-36">
+        <div className="sm:px-18  flex h-[50vh] flex-col-reverse items-center  bg-green-500  px-4 py-16  lg:px-24 xl:my-10 xl:flex-row  xl:px-36">
             <div className="container">
                 <div className="flex flex-1 flex-col items-center justify-center">
-                    <h1 className="text-center  font-bold text-yellow-500 md:mt-10 md:mb-5 ">
+                    <h1 className="text-center  text-2xl font-bold text-gray-100 pt-2 pb-2">
                         Highest Earners
                     </h1>
-                    <div className="flex justify-center w-full flex-col md:flex-row md:space-x-6">
-                     <div>
-                     <HighestEarnerCard/>
-                     </div>
-                    </div>
+                    <HighestEarnerCard />
                 </div>
             </div>
         </div>
