26d74d43a9df6ca0aa829467fe44d187e7b991b4
CodeDroid99 <johnmwega2002@gmail.com>
2024-01-08 15:06:51 +0300

    Set me up

--- pages/bid-assignments.tsx
+++ pages/bid-assignments.tsx
@@ -8,15 +8,26 @@ import {
   query,
   where,
 } from 'firebase/firestore';
-import { db } from '../firebase';
+import { auth, db } from '../firebase';
 import { formatDate } from './profile/[id]';
 import router from 'next/router';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const BidAssignments: React.FC = (props: any) => {
   const { assignments } = props;
   const handleNavigation = (assignmentId: string) => {
     router.push(`/order/${assignmentId}`);
   };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.push(`/login?redirect=/bid-assignment`)
+      }
+    })
+    return () => unsubscribe()
+  },)
+
 
   return (
     <>
