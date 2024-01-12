ea94756ce8636a6b8f3a46620c334ef45129ef42
CodeDroid99 <johnmwega2002@gmail.com>
2024-01-12 17:11:35 +0300

    reroute "admin/ to "/admin/

--- components/AdminDasboard/ApplicationsStatsCard.tsx
+++ components/AdminDasboard/ApplicationsStatsCard.tsx
@@ -80,7 +80,7 @@ const ApplicationsStatsCard = () => {
                 </div>
             </div>
             <div className="divide border-2 w-full mt-2 mb-2"></div>
-            <Link href="admin/manage-applications" className="flex align-items-right w-100">
+            <Link href="/admin/manage-applications" className="flex align-items-right w-100">
                 <span className="rounded bg-green-700 hover:bg-green-800 px-1 text-white">Manage</span>
             </Link>
         </div>
--- components/AdminDasboard/UsersStatsCard.tsx
+++ components/AdminDasboard/UsersStatsCard.tsx
@@ -70,7 +70,7 @@ const UsersStatsCard = () => {
                 </div>
             </div>
             <div className="divide border-2 w-full mt-2 mb-2"></div>
-            <Link href="admin/manage-users" className="flex align-items-right w-100">
+            <Link href="/admin/manage-users" className="flex align-items-right w-100">
                 <span className="rounded bg-green-700 hover:bg-green-800 px-1 text-white">Manage</span>
             </Link>
         </div>
