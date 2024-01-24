f48063803aa55df0a29adb5f36dddacbe2ece584
CodeDroid99 <johnmwega2002@gmail.com>
2024-01-24 22:32:30 +0300

    shsusha nyavu

--- pages/admin/manage-admins.tsx
+++ pages/admin/manage-admins.tsx
@@ -8,6 +8,8 @@ import UsersPage from '../../components/AdminDasboard/UsersTable';
 import SideNav from 'components/AdminDasboard/AdminSideNav'
 import Navbar from 'components/AdminLayout/Navbar'
 import AdminPage from 'components/AdminDasboard/AdminsTables'
+import Head from 'next/head'
+import AdminSideNav from 'components/AdminLayout/SideNav'
 
 
 
@@ -28,25 +30,45 @@ export default function ManageAdminDashboard() {
     }
     return (
 
-        <div className="flex h-screen overflow-hidden">
-            {/* Sidebar */}
-            <div className="w-1/6 bg-gray-100 h-full fixed top-0 left-0 overflow-y-auto mt-20">
-                <SideNav />
-            </div>
 
-            {/* Main Content */}
-            <div className="flex-grow mt-28 h-full overflow-y-auto">
-                <Navbar />
-                <div className=" mx-auto w-full ">
-                    <ImageHeader />
-                    <div className="row min-w-100 shadow-2xl">
-                        <div className="col-md-2"></div>
-                        <div className="col-md-10 rounded-xl">
-                            <AdminPage />
-                        </div>
+        <>
+            <Head>
+                <title>
+                    QualityUnitedWriters - Your Academic Research and Project Partner
+                </title>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta
+                    name="description"
+                    content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
+                />
+                <meta name="keywords" content="Academic writing services, Expert academic writers, Professional research assistance, High-quality research papers, Academic project support, Thesis and dissertation help, Essay writing service, Top-rated tutors, Academic success tips, Homework assistance, Online tutoring, Quality writing solutions, Best essay writers, Custom research papers, Academic support platform, Tutoring for students, Research paper editing, Writing and editing services, Academic guidance, Homework help for students" />
+                <meta name="author" content="QualityUnitedWriters" />
+                <meta name="robots" content="index, follow" />
+                <meta name="og:title" property="og:title" content="QualityUnitedWriters - Your Academic Research and Project Partner" />
+                <meta
+                    name="og:description"
+                    property="og:description"
+                    content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
+                />
+                <meta name="og:image" property="og:image" content="public/sync-my-socials-logo.png" />
+
+                <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
+            </Head>
+            <Navbar />
+            <div className="row flex pt-12 mt-8">
+                <div className="container flex">
+                    <div className="col-md-2 mx-auto ">
+                        <AdminSideNav />
+                    </div>
+                    <div className="col-md-10 mx-auto h-full pl-2 shadow-inner">
+                        <AdminPage />
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
+
+
+
