bdab944437a5e6c1c53defb2024404e2cf776d68
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-02 12:33:02 +0300

    questions

--- components/Homepage/HeroSection.tsx
+++ components/Homepage/HeroSection.tsx
@@ -1,7 +1,7 @@
 import Home from 'components/unAuthed/HomeSection';
 import Image from 'next/image'
 import Link from 'next/link';
-import HeroBackground from  "public/main_cover.jpg"
+import HeroBackground from "public/hero-bg.jpeg"
 import React from 'react';
 
 const HeroArea = () => {
@@ -9,12 +9,12 @@ const HeroArea = () => {
         <div className="hero_area">
             <div className="hero_bg_box">
                 <div className="img-box">
-                <Image
-                    src={HeroBackground}
-                    alt="assignment"
-                    
-                    className="w-full h-full"
-                  />
+                    <Image
+                        src={HeroBackground}
+                        alt="assignment"
+
+                        className="w-full h-full"
+                    />
                 </div>
             </div>
             <Home />
@@ -27,10 +27,10 @@ const HeroArea = () => {
                                     <div className="col-md-7">
                                         <div className="detail-box">
                                             <h1>
-                                                Get on-demand Q&A<span> Homework help</span> from verified tutors.
+                                                Get on-demand Q&A<span className="text-green-700"> Homework help</span> from verified tutors.
                                             </h1>
                                             <p className="pt-1 pb-2 text-lg">
-                                            Work with thousands of tutors to help you meet your deadlines and get the grades you so sorely need!
+                                                Work with thousands of tutors to help you meet your deadlines and get the grades you so sorely need!
                                             </p>
                                             <div className="btn-box whitespace-nowrap">
                                                 <Link href="/howitworks" className="btn-1 rounded"> Learn more </Link>
@@ -48,7 +48,7 @@ const HeroArea = () => {
                                         <div className="detail-box">
                                             <h1>
                                                 Apply for an account to start working online.
-                                                <span>
+                                                <span className="text-green-700">
                                                     Tutor Online </span>
                                             </h1>
                                             <p className="pt-1 pb-2 text-lg">Every 15 assigned tutoring, get 10% cash back (Limited to $50) on your posted assignments!</p>
@@ -67,7 +67,7 @@ const HeroArea = () => {
                                     <div className="col-md-7">
                                         <div className="detail-box">
                                             <h1>
-                                            APA examples/ samples<span> creative vision </span>
+                                                APA examples/ samples<span className="text-green-700"> creative vision </span>
                                             </h1>
                                             <p className="pt-1 pb-2 text-lg">
                                                 Collaborate to bring your ideas to life with precision, creativity, and innovation. We are your
@@ -81,7 +81,7 @@ const HeroArea = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div >
                     <div className="container idicator_container">
                         <ol className="carousel-indicators">
                             <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
@@ -89,9 +89,9 @@ const HeroArea = () => {
                             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                         </ol>
                     </div>
-                </div>
-            </section>
-        </div>
+                </div >
+            </section >
+        </div >
     );
 };
 
--- components/layout/HomeSection.tsx
+++ components/layout/HomeSection.tsx
@@ -6,13 +6,13 @@ import Image from 'next/image';
 const Home: React.FC = () => {
     return (
         <header id="home" className="header_section">
-             <div className="hero_bg_box">
+            <div className="hero_bg_box">
                 <div className="img-box">
-                <Image
-                     src="/public/hero-bg.jpg"
-                     width={200}
-                     height={140}
-                      alt="" />
+                    <Image
+                        src="/public/hero-bg.jpeg"
+                        width={200}
+                        height={140}
+                        alt="" />
                 </div>
             </div>
             <div className="">
