477c923dd561734cfd7c1a1e0a1fd28f3de5d7ab
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-03 13:49:17 +0300

    ad chatpage

---
+++ pages/chatpage.tsx
@@ -0,0 +1,15 @@
+// ChatPage.js
+
+import ChatComponent from 'components/layout/ChatComponent';
+import React from 'react';
+
+const ChatPage = () => {
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      <ChatComponent />
+    </div>
+  );
+};
+
+export default ChatPage;
