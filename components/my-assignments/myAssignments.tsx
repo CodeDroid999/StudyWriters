957ed236707db54f3d9bd12930dc7c863124ba64
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-29 17:53:55 +0300

    update myAssignments.tsx

--- components/my-assignments/myAssignments.tsx
+++ components/my-assignments/myAssignments.tsx
@@ -5,11 +5,11 @@ export default function MyAssignmentsDetails({ heading, assignments, warning }) 
   return (
     <div>
       {assignments.length === 0 ? (
-        <div className="mt-36 flex h-full flex-col items-center">
+        <div className="min-h-screen mt-36 flex h-full flex-col items-center">
           <h1 className="text-xl font-semibold text-gray-700">{warning}</h1>
         </div>
       ) : (
-        <div>
+        <div className="min-h-screen justify-items-center align-items-center">
           <h1 className="text-2xl font-medium text-green-950">{heading}</h1>
           <div className="mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {assignments.map((assignment: any) => (
