8910e2f5d4c3abeab79554c2994729766e95e1db
CodeDroid99 <johnmwega2002@gmail.com>
2024-03-03 21:41:15 +0300

    update [id].tsx

--- pages/support/[id].tsx
+++ pages/support/[id].tsx
@@ -14,6 +14,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { onAuthStateChanged } from 'firebase/auth'
 import NewMessage from 'components/supportChat/NewMessage'
+import { MdArrowBack } from 'react-icons/md'
 
 export default function Messages() {
   const router = useRouter()
@@ -69,77 +70,109 @@ export default function Messages() {
   }, [userId])
 
   return (
-    <div className="bg-green-950">
+    <div className="bg-green-900">
       <Navbar />
+
       {loading ? (
-        <div className="flex h-screen items-center justify-center">
-          <div
-            className="inline-block h-6 w-6 animate-spin rounded-full border-[3px] border-current border-t-transparent text-blue-600"
-            role="status"
-            aria-label="loading"
-          >
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
-      ) : supportChats.length === 0 ? (
-        <div className="flex h-screen items-center justify-center ">
-          <div className="text-lg font-semibold text-green-950 xl:text-2xl ">
-            You haven't sent any messages
-            <NewMessage customerId={userId} />
-          </div>
-        </div>
-      ) : (
-        <div className="mx-auto mt-24 max-w-[700px] px-3 h-screen bg-green-950 shadow-2xl">
-          <h1 className="mb-3 text-2xl font-semibold text-green-950">Customer Support</h1>
-          <ul>
-            {supportChats.map((chat: any) => (
-              <li
-                key={chat.chatId}
-                className="my-3 w-full rounded-xl bg-gray-200 px-1.5 py-2 md:px-2 md:py-4"
+        <>
+          <div className="fixed left-0 right-0 top-0  z-10 bg-white px-2 py-3 duration-300 ease-in">
+            <div className="mx-auto  max-w-[800px] ">
+              <div
+                onClick={() => router.back()}
+                className="flex w-[70px] cursor-pointer flex-row items-center justify-start "
               >
-                <Link href={`/chats/${chat.chatId}`}>
-                  <div className="flex flex-row items-center">
-                    <Image
-                      src={chat.otherParticipant?.profilePicture}
-                      width={50}
-                      height={50}
-                      alt="profile"
-                      className="h-[40px] w-[40px] rounded-full object-cover md:h-[50px] md:w-[50px]"
-                    />
-                    <div className="ml-1.5 flex flex-1 flex-col md:ml-3">
-                      <div className="flex  flex-row items-center justify-between">
-                        <span className="text-base font-medium text-green-950 md:text-lg">
-                          {chat.otherParticipant?.firstName}{' '}
-                          {chat.otherParticipant?.lastName}
-                        </span>
-                        <span className="text-xs font-medium text-gray-700">
-                          {new Date(
-                            chat.lastMessageTimestamp?.toMillis()
-                          ).toLocaleDateString('en-GB', {
-                            day: '2-digit',
-                            month: '2-digit',
-                            year: 'numeric',
-                          })}{' '}
-                          {new Date(
-                            chat.lastMessageTimestamp?.toMillis()
-                          ).toLocaleTimeString([], {
-                            hour: 'numeric',
-                            minute: '2-digit',
-                          })}
+                <MdArrowBack className="text-[20px] text-green-950" />
+                <span className="ml-1 text-[18px] font-medium text-green-950">
+                  Back
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex h-screen items-center justify-center">
+            <div
+              className="inline-block h-6 w-6 animate-spin rounded-full border-[3px] border-current border-t-transparent text-blue-600"
+              role="status"
+              aria-label="loading"
+            >
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+          ) : supportChats.length === 0 ? (
+          <div className="flex h-screen items-center justify-center ">
+            <div className="text-lg font-semibold text-green-950 xl:text-2xl ">
+              You haven't sent any messages
+              <NewMessage customerId={userId} />
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="fixed left-0 right-0 top-0  z-10 bg-white px-2 py-3 duration-300 ease-in">
+            <div className="mx-auto  max-w-[800px] ">
+              <div
+                onClick={() => router.back()}
+                className="flex w-[70px] cursor-pointer flex-row items-center justify-start "
+              >
+                <MdArrowBack className="text-[20px] text-green-950" />
+                <span className="ml-1 text-[18px] font-medium text-green-950">
+                  Back
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="mx-auto mt-24 max-w-[700px] px-3 h-screen bg-green-950 shadow-2xl">
+            <h1 className="mb-3 text-2xl font-semibold text-green-950">Customer Support</h1>
+            <ul>
+              {supportChats.map((chat: any) => (
+                <li
+                  key={chat.chatId}
+                  className="my-3 w-full rounded-xl bg-gray-200 px-1.5 py-2 md:px-2 md:py-4"
+                >
+                  <Link href={`/chats/${chat.chatId}`}>
+                    <div className="flex flex-row items-center">
+                      <Image
+                        src={chat.otherParticipant?.profilePicture}
+                        width={50}
+                        height={50}
+                        alt="profile"
+                        className="h-[40px] w-[40px] rounded-full object-cover md:h-[50px] md:w-[50px]"
+                      />
+                      <div className="ml-1.5 flex flex-1 flex-col md:ml-3">
+                        <div className="flex  flex-row items-center justify-between">
+                          <span className="text-base font-medium text-green-950 md:text-lg">
+                            {chat.otherParticipant?.firstName}{' '}
+                            {chat.otherParticipant?.lastName}
+                          </span>
+                          <span className="text-xs font-medium text-gray-700">
+                            {new Date(
+                              chat.lastMessageTimestamp?.toMillis()
+                            ).toLocaleDateString('en-GB', {
+                              day: '2-digit',
+                              month: '2-digit',
+                              year: 'numeric',
+                            })}{' '}
+                            {new Date(
+                              chat.lastMessageTimestamp?.toMillis()
+                            ).toLocaleTimeString([], {
+                              hour: 'numeric',
+                              minute: '2-digit',
+                            })}
+                          </span>
+                        </div>
+
+                        <span className="text-sm font-normal text-gray-700">
+                          {chat.lastMessage}
                         </span>
                       </div>
-
-                      <span className="text-sm font-normal text-gray-700">
-                        {chat.lastMessage}
-                      </span>
                     </div>
-                  </div>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )
+      }
+    </div >
   )
 }
